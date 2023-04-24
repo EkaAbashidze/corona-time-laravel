ad6b4a5a45c144a15b85a52bc768addb1b91dedf
Eka Abashidze <eka.abashidze@redberry.ge>
2023-04-24 19:23:14 +0400

    extract html head to new blade component

--- resources/views/components/wrapper.blade.php
+++ resources/views/components/wrapper.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @vite('resources/css/app.css')
-        <title>Laravel</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    </head>
-
-    <body>
+<x-html/>
       <div class="flex justify-between h-screen">
         <div class="flex flex-col pl-[108px] pt-[40px] w-auto">
           <img class="w-[170px]" src="{{ asset('storage/photos/logo.svg') }}" alt="Photo">
