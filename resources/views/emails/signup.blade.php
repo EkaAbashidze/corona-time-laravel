5165b3ac76b82e5b369b816ede0fc1839df2b8de
Eka Abashidze <eka.abashidze@redberry.ge>
2023-05-10 13:00:20 +0400

    fix: make email styling work

--- resources/views/emails/signup.blade.php
+++ resources/views/emails/signup.blade.php
@@ -1,16 +1,80 @@
-<x-mail::message>
-    <img src="{{ asset('storage/photos/email-image.png') }}" style="border: 2px solid #0FBA68; border-radius: 16px; width: 520px; height: 365px;" />
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: auto;
+        }
 
-    <h1 style="font-family: 'Inter'; font-style: normal; font-weight: 900; font-size: 25px; line-height: 30px; text-align: center; color: #010414;">Confirmation Email</h1>
+        .email-image {
+            border: 2px solid #0FBA68;
+            border-radius: 16px;
+            width: 520px;
+            height: 365px;
+            display: block;
+            margin: auto;
+        }
 
-    <p style="font-family: 'Inter'; font-style: normal; font-weight: 400; font-size: 18px; line-height: 22px; text-align: center; color: #010414;">Click the button below to verify your email.</p>
+        .title {
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 900;
+            font-size: 25px;
+            line-height: 30px;
+            text-align: center;
+            color: #010414;
+        }
 
-    <x-mail::button :url="route('verification.verify')" style="display: block; width: 392px; height: 56px; background: #0FBA68; border-radius: 8px; text-align: center; font-size: 18px; line-height: 56px; font-weight: bold; text-decoration: none; color: #FFFFFF;">
-    Verify Email
-    </x-mail::button>
+        .message {
+            font-family: 'Inter';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 18px;
+            line-height: 22px;
+            text-align: center;
+            color: #010414;
+        }
+
+        .verify-button {
+            display: block;
+            width: 392px;
+            height: 56px;
+            background: #0FBA68;
+            border-radius: 8px;
+            text-align: center;
+            font-size: 18px;
+            line-height: 56px;
+            font-weight: bold;
+            text-decoration: none;
+            color: #FFFFFF;
+            margin: auto;
+        }
+
+        .center {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div>
+
+    
+        <img src="{{ asset('storage/photos/email-image.png') }}" class="email-image" />
+
+        <h1 class="title">{{ __('messages.confirmation_email') }}</h1>
+
+        <p class="message">{{ __('messages.verify_email') }}</p>
 
 
-    <p style="text-align: center;">Thanks,<br>
-        {{ config('app.name') }}
-    </p>
-</x-mail::message>
+    <a href="route('verification.verify')" class="verify-button">
+    {{ __('messages.verify_email_button') }}
+    </a>
+
+    </div>
+</body>
+</html>
