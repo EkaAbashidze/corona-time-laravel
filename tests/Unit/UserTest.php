a5e8026b7d5216baac6f3c0e1bc75994d34cda6a
Eka Abashidze <eka.abashidze@redberry.ge>
2023-05-23 10:01:55 +0400

    feat: add user tests

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+	public function test_login_form()
+	{
+		$response = $this->get('/login');
+
+		$response->assertStatus(200);
+	}
+
+	public function test_user_duplication()
+	{
+		$user1 = User::factory()->create([
+			'username' => fake()->userName(),
+			'email'    => fake()->unique()->safeEmail(),
+		]);
+
+		$user2 = User::factory()->create([
+			'username' => fake()->userName(),
+			'email'    => fake()->unique()->safeEmail(),
+		]);
+
+		$this->assertTrue($user1->username != $user2->username);
+	}
+
+	public function test_delete_user()
+	{
+		$user = User::factory()->count(1)->make();
+
+		$user = User::first();
+
+		if ($user) {
+			$user->delete();
+		}
+
+		$this->assertTrue(true);
+	}
+
+	public function test_if_stores_new_users()
+	{
+		$response = $this->post('/signup', [
+			'username'              => fake()->userName(),
+			'email'                 => fake()->unique()->safeEmail(),
+			'password'              => 'password',
+			'password_confirmation' => 'password',
+			'remember_device'       => false,
+		]);
+		$response->assertRedirect('/confirmation');
+	}
+
+	public function test_database()
+	{
+		$this->assertDatabaseHas('users', [
+			'username' => 'Elena',
+		]);
+	}
+
+	public function test_if_seeders_works()
+	{
+		$this->seed();
+	}
+
+	// USER INPUT
+
+	public function test_user_input_validation()
+	{
+		// Test for input fields
+		$response = $this->post('/signup', []);
+		$response->assertSessionHasErrors(['username', 'email', 'password']);
+
+		// valid email
+		$response = $this->post('/signup', [
+			'username'              => fake()->userName(),
+			'email'                 => 'invalidemail',
+			'password'              => fake()->password(),
+			'password_confirmation' => fake()->password(),
+			'remember_device'       => false,
+		]);
+		$response->assertSessionHasErrors(['email']);
+
+		// password
+
+		$response = $this->post('/signup', [
+			'username'              => fake()->userName(),
+			'email'                 => fake()->unique()->safeEmail(),
+			'password'              => fake()->password(),
+			'password_confirmation' => fake()->password(),
+		]);
+		$response->assertSessionHasErrors(['password']);
+
+		// Unique email
+
+		$user = User::factory()->create();
+		$response = $this->post('/signup', [
+			'username'              => 'JohnDoe',
+			'email'                 => $user->email,
+			'password'              => 'password',
+			'password_confirmation' => 'password',
+		]);
+
+		$response->assertSessionHasErrors(['email']);
+	}
+
+	public function test_user_authentication()
+	{
+		// TODO
+	}
+
+	public function test_password_hashing()
+	{
+		$password = 'password';
+		$user = User::factory()->create(['password' => bcrypt($password)]);
+		$this->assertTrue(Hash::check($password, $user->password));
+	}
+
+	public function test_email_sending()
+	{
+		// TODO
+	}
+}
