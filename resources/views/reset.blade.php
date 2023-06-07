6bee8fc02b83da37b42d2c8ca94ee23facd1096c
Eka Abashidze <eka.abashidze@redberry.ge>
2023-06-07 18:45:17 +0400

    fix/style: add border red to show error on reset password page

--- resources/views/reset.blade.php
+++ resources/views/reset.blade.php
@@ -7,7 +7,7 @@
     @csrf
     <div class="mt-4">
       <label class="font-bold text-gray-dark text-[16px]" for="email">{{ __('messages.email') }}</label>
-      <input type="email" name="email" id="email" placeholder="Enter your email" class="block h-[56px] w-[390px] mt-2 px-4 py-2 rounded-md bg-white border border-gray-300 focus:border-brand-green focus:ring-2 focus:ring-brand-green focus:ring-opacity-50" value="{{ old('email') }}" required autocomplete="email">
+      <input type="email" name="email" id="email" placeholder="Enter your email" class="block h-[56px] w-[390px] mt-2 px-4 py-2 rounded-md bg-white border border-gray-300 {{ $errors->has('email') ? 'border-red-500' : 'border-gray-300' }}" value="{{ old('email') }}" required autocomplete="email">
     </div>
     @error('email')
        <p class="text-red-500 text-sm mt-1">{{ $message }}</p>
