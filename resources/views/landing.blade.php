9f0fa36642c91ed0e6752989e479c33929bcbc01
Eka Abashidze <eka.abashidze@redberry.ge>
2023-06-07 19:59:32 +0400

    fix/style: make landing page responsive

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -40,35 +40,48 @@
       @endphp
   @endforeach
 
-<div class="flex gap-x-6">
-  <div class="relative">
-    <div class="w-[392px] h-[255px] left-108 top-265 bg-[#2029F3] opacity-[0.08] shadow-card rounded-2xl flex flex-col items-center justify-center">
-    </div>
-    <div class="absolute top-0 left-0 translate-y-10 translate-x-3/4 flex flex-col items-center align-center justify-center">
+<div class="flex gap-6 justify-between max-[600px]:justify-start w-full max-[600px]:flex-col ">
+
+  <div class="">
+    <div class="max-[600px]:w-[343px] max-[600px]:h-[192px] w-[392px] h-[255px] left-108 top-265 bg-[#2029F3] bg-opacity-[0.08] shadow-card rounded-2xl flex flex-col items-center justify-center">
+
+    <div class="flex flex-col items-center align-center justify-center">
       <img src="{{ asset('images/photos/new.svg') }}" alt="Photo" class="w-[90px]">
       <p class="font-inter font-medium text-[#010414] text-center text-lg leading-6 mt-6">{{ __('messages.new_cases') }}</p>
       <h2 class="font-inter font-extrabold text-[#2029F3] text-4xl leading-12 mt-4">{{ $totalConfirmed }}</h2>
+      </div>
     </div>
  </div>
-<div class="relative">
-  <div class="w-[392px] h-[255px] left-108 top-265 bg-[#0FBA68] opacity-[0.08] shadow-card rounded-2xl flex flex-col items-center justify-center">
-  </div>
-  <div class="absolute top-0 left-0 translate-y-12 translate-x-3/4 flex flex-col items-center align-center justify-center">
+
+<div class="flex gap-x-6 flex-row justify-between w-full">
+
+<div class="mx-auto">
+  <div class="max-[600px]:w-[164px] max-[600px]:h-[192px] w-[392px] h-[255px] left-108 top-265 bg-[#0FBA68] bg-opacity-[0.08] shadow-card rounded-2xl flex flex-col items-center justify-center">
+
+  
+  <div class="flex flex-col items-center align-center justify-center">
     <img src="{{ asset('images/photos/recovered.svg') }}" alt="Photo" class="w-[90px]">
     <p class="font-inter font-medium text-[#010414] text-center text-lg leading-6 mt-6">{{ __('messages.recovered') }}</p>
     <h2 class="font-inter font-extrabold text-[#0FBA68] text-4xl leading-12 mt-4">{{ $totalRecovered }}</h2>
+    </div>
   </div>  
 </div>
-  <div class="relative">
-    <div class="w-[392px] h-[255px] left-108 top-265 bg-[#EAD621] opacity-[0.08] shadow-card rounded-2xl flex flex-col items-center justify-center">
-    </div>
-    <div class="absolute top-0 left-0 translate-y-11 translate-x-3/4 flex flex-col items-center align-center justify-center">
+
+  <div class="">
+    <div class="max-[600px]:w-[164px] max-[600px]:h-[192px] w-[392px] h-[255px] left-108 top-265 bg-[#EAD621] bg-opacity-[0.08] shadow-card rounded-2xl flex flex-col items-center justify-center">
+
+    <div class="flex flex-col items-center align-center justify-center">
       <img src="{{ asset('images/photos/deaths.svg') }}" alt="Photo" class="w-[90px]">
       <p class="font-inter font-medium text-[#010414] text-center text-lg leading-6 mt-6">{{ __('messages.deaths') }}</p>
       <h2 class="font-inter font-extrabold text-[#EAD621] text-4xl leading-12 mt-4">{{ $totalDeaths }}</h2>
+      </div>
     </div> 
   </div>
+
+  </div>
+
 </div>
+
 @endif
 @if(request('view') === 'bycountry')
 <form method="GET" action="{{ route('landing') }}" class="mb-5">
