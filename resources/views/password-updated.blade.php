d3aa01f5429c40382ff316481f4ac3a749c64717
Eka Abashidze <eka.abashidze@redberry.ge>
2023-06-12 14:20:32 +0400

    style: make the rest pages responsive

--- resources/views/password-updated.blade.php
+++ resources/views/password-updated.blade.php
@@ -1,6 +1,6 @@
 <x-html/>
 <div class="flex flex-col items-center h-screen mt-[40px]">
-  <img class="w-[170px] mb-8" src="{{ asset('images/photos/logo.svg') }}" alt="Photo">
+  <img class="self-start md:self-center md:w-[170px] w-[137px] mb-8 src="{{ asset('images/photos/logo.svg') }}" alt="Photo">
   <div class="text-center flex flex-col items-center justify-center h-screen gap-y-4">
     <img src="{{ asset('images/gif/checkmark.gif') }}" alt="gif" class="hidden sm:block">
     <h1 class="">{{ __('messages.password-updated') }}!</h1>
