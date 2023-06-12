537b7effeb336967e8ab675a14dfe1c6d0e2ba70
Eka Abashidze <eka.abashidze@redberry.ge>
2023-06-12 12:02:36 +0400

    style: add responsive design to signup page

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,12 +1,12 @@
 @extends('components.wrapper')
 
 @section('content')
-<h1 class="text-gray-dark font-bold text-[25px] mt-[60px]">{{ __('messages.welcome') }}</h1>
+<h1 class="text-gray-dark font-bold text-[20px] md:text-[25px] mt-[60px]">{{ __('messages.welcome') }}</h1>
 <form method="POST" action="{{ route('signup.store') }}" class="mt-8">
   @csrf
   <div class="mt-4">
-    <label class="font-bold text-gray-dark text-[16px]" for="username">{{ __('messages.username') }}</label>
-    <input type="text" name="username" id="username" placeholder="Enter unique username" class="block h-[56px] w-full mt-2 px-4 py-2 rounded-md bg-white border border-gray-300 {{ $errors->has('username') ? 'border-red-500' : 'border-gray-300' }}" value="{{ old('username') }}" required autocomplete="username" autofocus>
+    <label class="font-bold text-gray-dark text-sm md:text-base " for="username">{{ __('messages.username') }}</label>
+    <input type="text" name="username" id="username" placeholder="Enter unique username" class="block h-[56px] w-[343px] md:w-full mt-2 px-4 py-2 rounded-md bg-white border border-gray-300 {{ $errors->has('username') ? 'border-red-500' : 'border-gray-300' }}" value="{{ old('username') }}" required autocomplete="username" autofocus>
   </div>
 
   @error('username')
@@ -14,8 +14,8 @@
   @enderror
 
   <div class="mt-4">
-    <label class="font-bold text-gray-dark text-[16px]" for="email">{{ __('messages.email') }}</label>
-    <input type="email" name="email" id="email" placeholder="Enter your email" class="block h-[56px] w-full mt-2 px-4 py-2 rounded-md bg-white border border-gray-300 {{ $errors->has('email') ? 'border-red-500' : 'border-gray-300' }}" value="{{ old('email') }}" required autocomplete="email">
+    <label class="font-bold text-gray-dark text-sm md:text-base" for="email">{{ __('messages.email') }}</label>
+    <input type="email" name="email" id="email" placeholder="Enter your email" class="block h-[56px] w-[343px] md:w-full mt-2 px-4 py-2 rounded-md bg-white border border-gray-300 {{ $errors->has('email') ? 'border-red-500' : 'border-gray-300' }}" value="{{ old('email') }}" required autocomplete="email">
   </div>
 
   @error('email')
@@ -23,13 +23,13 @@
   @enderror
 
   <div class="mt-4">
-    <label class="font-bold text-gray-dark text-[16px]" for="password">{{ __('messages.password') }}</label>
-    <input type="password" name="password" id="password" placeholder="Enter your password" class="block h-[56px] w-full mt-2 px-4 py-2 rounded-md bg-white border border-gray-300 {{ $errors->has('password') ? 'border-red-500' : 'border-gray-300' }}" required autocomplete="new-password">
+    <label class="font-bold text-gray-dark text-sm md:text-base" for="password">{{ __('messages.password') }}</label>
+    <input type="password" name="password" id="password" placeholder="Enter your password" class="block h-[56px] w-[343px] md:w-full mt-2 px-4 py-2 rounded-md bg-white border border-gray-300 {{ $errors->has('password') ? 'border-red-500' : 'border-gray-300' }}" required autocomplete="new-password">
   </div>
 
   <div class="mt-4">
-    <label class="font-bold text-gray-dark text-[16px]" for="password_confirmation">{{ __('messages.repeat_password') }}</label>
-    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Repeat your password" class="block h-[56px] w-full mt-2 px-4 py-2 rounded-md bg-white border border-gray-300 {{ $errors->has('password') ? 'border-red-500' : 'border-gray-300' }}" required autocomplete="new-password">
+    <label class="font-bold text-gray-dark text-sm md:text-base" for="password_confirmation">{{ __('messages.repeat_password') }}</label>
+    <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Repeat your password" class="block h-[56px] w-[343px] md:w-full mt-2 px-4 py-2 rounded-md bg-white border border-gray-300 {{ $errors->has('password') ? 'border-red-500' : 'border-gray-300' }}" required autocomplete="new-password">
   </div>
 
   @error('password')
@@ -42,8 +42,8 @@
   </div>
 
   <div class="flex flex-col mt-8 text-center">
-    <button type="submit" class="h-[56px] bg-brand-green text-white py-2 rounded-md font-bold text-center mb-8">{{ __('messages.signup') }}</button>
-    <p class="text-gray-700 text-base">{{ __('messages.have_account') }} <a href="{{ route('login') }}" class="font-bold">
+    <button type="submit" class="h-[56px] bg-brand-green text-white py-2 rounded-md font-bold text-center mb-8 w-[343px] md:w-full text-sm md:text-base">{{ __('messages.signup') }}</button>
+    <p class="text-gray-700 text-base w-[343px] md:w-full text-sm md:text-base">{{ __('messages.have_account') }} <a href="{{ route('login') }}" class="font-bold">
       {{ __('messages.login') }}</a></p>
   </div>
 </form>
